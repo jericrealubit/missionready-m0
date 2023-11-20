d5492645a3798097a1c87fe0c2b1ac181995274f
jeric realubit <jericrealubit@gmail.com>
2023-11-20 19:59:22 +1300

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=8c4efb6b";
+const API_URL = "https://www.omdbapi.com?apikey=8c4efb6b";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
